9716d90f9abfe290d786cadd500f01479534d0df
josef.ludvicek.cz <josef.ludvicek.cz@3c8b43d0-41d9-6fe7-6c1e-da1108b34719>
2012-05-12 10:20:26 +0000

    Added class Validator, which can determine user identification type from string - if its e-mail, flickr id...

---
+++ src/java/cz/muni/fi/pb138/flickrgraphr/api/Validator.java
@@ -0,0 +1,71 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.muni.fi.pb138.flickrgraphr.api;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import org.apache.commons.validator.routines.EmailValidator;
+
+/**
+ * This class can recognize type of user identification
+ * @author mantaexx
+ */
+public class Validator {
+
+    /**
+     * Enum with supported id types
+     */
+    public enum IdType {
+
+        flickrId, name, email, invalidId
+    }
+    
+    /**
+     * This method recognizes id type from string
+     * @param id string with user identification - eg. display name or e-mail
+     * @return id type, or invalidId
+     */
+    public static IdType getIdType(String id) {
+
+        if (id == null && id.equals("")) {
+            return IdType.invalidId;
+        }
+
+        if (isEmail(id)) {
+            return IdType.email;
+        }
+
+        if (isUserId(id)) {
+            return IdType.flickrId;
+        }
+        
+        if(id.length() > 3)
+            return IdType.name;
+
+        return IdType.invalidId;
+    }
+
+    /**
+     * Determines if given string is e-mail address
+     * @param id string with e-mail
+     * @return true if given string is e-mail
+     */
+    public static boolean isEmail(String id) {
+        EmailValidator eVal = EmailValidator.getInstance();
+        return eVal.isValid(id);
+    }
+    
+    /**
+     * Determines if given string is flickr user ID
+     * @param id string with id
+     * @return returns true if given string is sth. like 12345678@N12
+     */
+    public static boolean isUserId(String id) {
+        Pattern uIdPattern = Pattern.compile("\\d{8}[@][A-Z]\\d{2}");
+        Matcher matcher = uIdPattern.matcher(id);
+
+        return matcher.matches();
+    }
+}
