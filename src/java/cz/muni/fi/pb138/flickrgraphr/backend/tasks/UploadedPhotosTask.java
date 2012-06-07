b78e300d11da1293305d6fc7d3f5789d6bda3df7
repli2dev@gmail.com <repli2dev@gmail.com@3c8b43d0-41d9-6fe7-6c1e-da1108b34719>
2012-06-07 10:47:05 +0000

    - Add forgotten task

---
+++ src/java/cz/muni/fi/pb138/flickrgraphr/backend/tasks/UploadedPhotosTask.java
@@ -0,0 +1,36 @@
+package cz.muni.fi.pb138.flickrgraphr.backend.tasks;
+
+import cz.muni.fi.pb138.flickrgraphr.backend.cron.TaskInContext;
+import cz.muni.fi.pb138.flickrgraphr.flickr.api.FlickrEntity;
+import cz.muni.fi.pb138.flickrgraphr.flickr.api.FlickrEntityException;
+import cz.muni.fi.pb138.flickrgraphr.flickr.api.TopTags;
+import cz.muni.fi.pb138.flickrgraphr.flickr.api.UploadedPhotos;
+import cz.muni.fi.pb138.flickrgraphr.tools.DateTimeHelper;
+import it.sauronsoftware.cron4j.TaskExecutionContext;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * Task to download count of uploaded photos
+ *
+ * @author Jan Drabek
+ */
+public class UploadedPhotosTask extends TaskInContext {
+
+	private static final Logger logger = Logger.getLogger(UploadedPhotosTask.class.getName());
+
+	@Override
+	public void execute(TaskExecutionContext tec) throws RuntimeException {
+		if (context == null) {
+			throw new RuntimeException("No context, cannot continue;");
+		}
+		FlickrEntity entity = new UploadedPhotos(context, DateTimeHelper.yesterday());
+		try {
+			entity.load();
+			logger.log(Level.INFO, "Processing of 'count of uploaded photos' finished.");
+		} catch (FlickrEntityException ex) {
+			logger.log(Level.SEVERE, "Downloading of 'count of uploaded photos' failed.", ex);
+		}
+
+	}
+}
